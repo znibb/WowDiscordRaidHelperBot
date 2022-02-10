4d3b9027f62301d4d74d6358adb8383bed256be7
znibb <znibb@pegersson.se>
2022-02-10 09:11:43 +0100

    Redirected both stdout and stderr to logfile

--- release.sh
+++ release.sh
@@ -23,33 +23,33 @@ fi
 # Update version number
 echo "Updating version number (1/$logsteps)"
 sed -i "s/botVersion=\".*\"/botVersion=\"$version\"/" bot.py
-git add bot.py >> $logfile
-git commit -m "Updated displayed version number to $version" >> $logfile
+git add bot.py >> $logfile 2>&1
+git commit -m "Updated displayed version number to $version" >> $logfile 2>&1
 
 # Push updates and merge with master
 echo "Push updates and merge with master (2/$logsteps)"
-git push origin develop >> $logfile
-git checkout main >> $logfile
-git merge develop >> $logfile
-git push origin main >> $logfile
+git push origin develop >> $logfile 2>&1
+git checkout main >> $logfile 2>&1
+git merge develop >> $logfile 2>&1
+git push origin main >> $logfile 2>&1
 
 # Create and push tag for new version
 echo "Create and push tag for new version (3/$logsteps)"
-git tag -a $version -m "Release $version" >> $logfile
-git push origin $version >> $logfile
+git tag -a $version -m "Release $version" >> $logfile 2>&1
+git push origin $version >> $logfile 2>&1
 
 # Return to development branch
 echo "Return to development branch (4/$logsteps)"
-git checkout develop >> $logfile
-git merge main >> $logfile
-git push origin develop >> $logfile
+git checkout develop >> $logfile 2>&1
+git merge main >> $logfile 2>&1
+git push origin develop >> $logfile 2>&1
 
 # Build and push new docker images
 echo "Build and push new docker images (5/$logsteps)"
-./build.sh >> $logfile
-docker tag $user/$name:latest $user/$name:$version >> $logfile
-docker push $user/$name:$version >> $logfile
-docker push $user/$name:latest >> $logfile
+./build.sh >> $logfile 2>&1
+docker tag $user/$name:latest $user/$name:$version >> $logfile 2>&1
+docker push $user/$name:$version >> $logfile 2>&1
+docker push $user/$name:latest >> $logfile 2>&1
 
 # Exit gracefully
 echo "Done!"
