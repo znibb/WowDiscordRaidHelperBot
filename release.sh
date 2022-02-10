902ddad7231b23c400b6154bf643f6d9f3a63573
znibb <znibb@pegersson.se>
2022-02-10 09:02:41 +0100

    Reduced verbosity and moved full output to logfile

--- release.sh
+++ release.sh
@@ -4,6 +4,11 @@ version=$1
 current_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 user=znibb
 name=raidhelper
+logfile=output.log
+logsteps=5
+
+# Start log file
+echo "Releasing $1 at $(date)" > $logfile
 
 # Check that script was run with argument
 if [[ -z $1 ]]; then
@@ -16,30 +21,36 @@ elif [[ $current_branch != "develop" ]]; then
 fi
 
 # Update version number
+echo "Updating version number (1/$logsteps)"
 sed -i "s/botVersion=\".*\"/botVersion=\"$version\"/" bot.py
-git add bot.py
-git commit -m "Updated displayed version number to $version"
+git add bot.py >> $logfile
+git commit -m "Updated displayed version number to $version" >> $logfile
 
 # Push updates and merge with master
-git push origin develop
-git checkout main
-git merge develop
-git push origin main
+echo "Push updates and merge with master (2/$logsteps)"
+git push origin develop >> $logfile
+git checkout main >> $logfile
+git merge develop >> $logfile
+git push origin main >> $logfile
 
 # Create and push tag for new version
-git tag -a $version -m "Release $version"
-git push origin $version
+echo "Create and push tag for new version (3/$logsteps)"
+git tag -a $version -m "Release $version" >> $logfile
+git push origin $version >> $logfile
 
 # Return to development branch
-git checkout develop
-git merge main
-git push origin develop
+echo "Return to development branch (4/$logsteps)"
+git checkout develop >> $logfile
+git merge main >> $logfile
+git push origin develop >> $logfile
 
 # Build and push new docker images
-./build.sh
-docker tag $user/$name:latest $user/$name:$version
-docker push $user/$name:$version
-docker push $user/$name:latest
+echo "Build and push new docker images (5/$logsteps)"
+./build.sh >> $logfile
+docker tag $user/$name:latest $user/$name:$version >> $logfile
+docker push $user/$name:$version >> $logfile
+docker push $user/$name:latest >> $logfile
 
 # Exit gracefully
+echo "Done!"
 exit 0
